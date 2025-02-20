5a181c1e94b998103fd0bb87dc0bf480f83ead2d
Earnest Piolo Fano <earnestpiolofano.developer@gmail.com>
2025-02-21 03:22:26 +0800

    Added Dynamic Adding of HTML Element based on Data retrieved from File

---
+++ scripts/script.js
@@ -0,0 +1,29 @@
+fetch ("../data.json")
+    .then(rawData => rawData.text())
+    .then(transformedData => {
+        const parsedData = JSON.parse(transformedData);
+        const summaryContent = document.getElementById("summaryContent");
+
+        for (const [i, result] of parsedData.entries()) {
+            const summaryDetail = document.createElement("div");
+            const summaryLogo = document.createElement("img");
+            const summaryDescription = document.createElement("p");
+            const summaryRatio = document.createElement("p");
+            const summaryRatioConsequent = document.createElement("span");
+
+            summaryDetail.classList.add("summary__details", `summary__details--${i}`);
+            summaryLogo.src = result.icon;
+            summaryLogo.classList.add("summary__details__logo");
+            summaryDescription.innerText = result.category;
+            summaryRatio.classList.add("summary__details__ratio");
+            summaryRatio.innerText = `${result.score} / `;
+            summaryRatioConsequent.classList.add("summary__details__consequent");
+            summaryRatioConsequent.innerText = "100";
+
+            summaryRatio.appendChild(summaryRatioConsequent);
+            summaryDetail.appendChild(summaryLogo);
+            summaryDetail.appendChild(summaryDescription);
+            summaryDetail.appendChild(summaryRatio);
+            summaryContent.appendChild(summaryDetail);
+        }
+    });
